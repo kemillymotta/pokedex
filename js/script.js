b3facc4e2a27a0e2167702ca815a7ff712770651
Kemilly Motta <kemillymotta31@gmail.com>
2026-08-06 19:55:26 -0300

    feat: add if statement for no response case

--- js/script.js
+++ js/script.js
@@ -8,19 +8,25 @@ const input = document.querySelector('.input_search');
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+
+    if (APIResponse.status === 200) {
     const data = await APIResponse.json();
     return data;
+  }
 }
 
     const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    
-    input.value = '';
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        
+        input.value = '';
+    }
+
 }
 
 form.addEventListener('submit', (event) => {
